051ccacc601f4eaada6348e68e728a7ca890a6e3
randihi <randihigashi@gmail.com>
2018-06-14 15:55:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,14 +1,20 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
+# class App < Sinatra::Base
 
+#   get '/food_form' do
+#     erb :food_form
+#   end
+
+#   # Add your post route and action below
+#   post '/food' do
+#     params.to_s
+#   "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+#   end
+# end
+
+class App < Sinatra::Base
   get '/food_form' do
     erb :food_form
-  end
-
-  # Add your post route and action below
-  post '/food' do
-    params.to_s
-   "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
   end
 end
